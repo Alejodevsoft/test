dae9cce3f64986f6c780e2566eca25cf0d31559c
Alejodevsoft <alejodevsoft@gmail.com>
2024-08-19 18:44:59 -0500

    Actualización de info en docusign

--- routes.php
+++ routes.php
@@ -6,4 +6,5 @@ return [
     'test2'         => 'App\Controllers\MainController::test2',
     'additional'    => 'App\Controllers\AdditionalController',
     'jwt-verify'    => 'App\Controllers\MainController::jwt',
+    'save-docusign' => 'App\Controllers\MainController::saveDocusign',
 ];
