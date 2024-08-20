3752ebc7baa238e95d36218fa77a4d7cc7895fbd
devSmithJTA <dev.smithjta@gmail.com>
2024-08-19 19:42:04 -0500

    verify jwt

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -6,6 +6,10 @@ use App\Libs\AesClass;
 use App\Libs\Monday;
 use App\Models\MainModel;
 use DocuSign\eSign\Client\ApiClient;
+use DocuSign\eSign\Model\TemplateRole;
+use DocuSign\eSign\Api\EnvelopesApi;
+use DocuSign\eSign\Configuration;
+use DocuSign\eSign\Model\EnvelopeDefinition;
 
 class MainController{
     public function index(){
@@ -78,7 +82,6 @@ class MainController{
                     $oauthBasePath = 'account.docusign.com';
                 }
                 
-                $expiresIn = 3600;
 
                 $apiClient = new ApiClient();
                 $apiClient->getOAuth()->setOAuthBasePath($oauthBasePath);
@@ -105,5 +108,94 @@ class MainController{
         }else{
             header('Location: ./');
         }
+    }
+    public function send(){
+        $datamonday = json_decode(file_get_contents('php://input'));
+
+        $model = new MainModel();
+        $clients = $model->getClientByMondayId($datamonday->payload->inputFields->userId);
+
+        if (!empty($clients)) {
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://api.monday.com/v2',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => array('query' => "{items(ids: [".$datamonday->payload->inputFields->itemId."]){column_values(types: text){text}subitems{name,column_values(types: email){text}}}}"),
+                CURLOPT_HTTPHEADER => array(
+                  "Authorization: ".AesClass::decrypt($clients['api_key_monday'])
+                ),
+            ));
+
+            $responseMonday = json_decode(curl_exec($curl));
+
+            curl_close($curl);
+
+            $clientId = AesClass::decrypt($clients['client_id_docusign']);
+            $userId = AesClass::decrypt($clients['user_id_docusign']);
+            if ($clients['server_docusign'] == 0) {
+                $oauthBasePath = 'account-d.docusign.com';
+            } else {
+                $oauthBasePath = 'account.docusign.com';
+            }
+            $apiClient = new ApiClient();
+            $apiClient->getOAuth()->setOAuthBasePath($oauthBasePath);
+            $privateKey = AesClass::decrypt($clients['private_key']);
+
+            $jwt_scope = 'signature';
+
+            $response = $apiClient->requestJWTUserToken(
+                $clientId,
+                $userId,
+                $privateKey,
+                $jwt_scope
+            );
+
+            $accessToken = $response[0]->getAccessToken();
+            $accountId = $apiClient->getUserInfo($response[0]->getAccessToken())[0]["accounts"][0]["account_id"];
+
+            
+            if ($clients['server_docusign'] == 0) {
+                $basePath = 'https://demo.docusign.net/restapi';
+            } else if($clients['server_docusign'] > 0 && $clients['server_docusign'] < 5){
+                $number = $clients['server_docusign'];
+                $basePath = "https://na$number.docusign.net/restapi";
+            } elseif($clients['server_docusign'] == 5){
+                $basePath = "https://ca.docusign.net/restapi";
+            } elseif($clients['server_docusign'] == 6){
+                $basePath = "https://au.docusign.net/restapi";
+            } elseif($clients['server_docusign'] == 7){
+                $basePath = "https://eu.docusign.net/restapi";
+            }
+
+
+        }
+       
+        $configuration = new Configuration();
+        $configuration->setHost($basePath);
+        $configuration->addDefaultHeader('Authorization', 'Bearer ' . $accessToken);
+    
+        $apiClient = new ApiClient($configuration);
+        $envelopeApi = new EnvelopesApi($apiClient);
+        $signers = [];
+        foreach ($responseMonday->data->items[0]->subitems as $key => $signer ) {
+            $signers[$key] = new TemplateRole([
+                'email' => $signer->column_values[0]->text,
+                'name' => $signer->name,
+                'role_name' => 'Signer'.($key+1)
+            ]);
+        }
+    
+        $envelopeDefinition = new EnvelopeDefinition([
+            'template_id' => $responseMonday->data->items[0]->column_values[0]->text,
+            'template_roles' => $signers,
+            'status' => 'sent'
+        ]);
     }
 }
