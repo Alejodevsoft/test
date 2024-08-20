37d3b17b6ed73229bc6720676ad2edac27e95e81
Alejodevsoft <alejodevsoft@gmail.com>
2024-08-19 20:15:20 -0500

    Common name en index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/common.php';
+require_once __DIR__ . '/Common.php';
 
 $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
