003685a7f2e5e39206e5aa70889e78265ebc391c
devSmithJTA <dev.smithjta@gmail.com>
2024-11-23 05:53:24 -0500

    Fase 1 mutación id de template

--- src/js/views/templates.js
+++ src/js/views/templates.js
@@ -1,12 +1,14 @@
 var select = new SlimSelect({
     select: '#boardSelect'
-})
+});
 const loader = document.getElementById('loader');
-const error = document.getElementById('error');
+const errordiv = document.getElementById('error');
 
 document.getElementById('boardSelect').addEventListener("change", (event) => {
+    document.getElementById('bodyTable').innerHTML = "";
+    document.querySelector('.table').classList.add('hidden');
     loader.classList.remove('hidden');
-    error.classList.add('hidden');
+    errordiv.classList.add('hidden');
     // alert(select.getSelected());
     fetch(getUrl()+"admin/contracts?board_id="+select.getSelected(), {
         method: 'GET',
@@ -21,41 +23,89 @@ document.getElementById('boardSelect').addEventListener("change", (event) => {
         return response.json();
     })
     .then(data => {
-        if (data.success) {
-            // select.disable();
+        var dataMonday = data;
+        if (dataMonday.success) {
             var html = "";
-            data.data.forEach(contract => {
-                if (contract.column_values[0].text) {
-                    html += `
-                        <tr>
-                            <td>${contract.name}</td>
-                            <td>existe</td>
-                        </tr>
-                    `
-                } else {
-                    html += `
-                        <tr>
-                            <td>${contract.name}</td>
-                            <td>no existe</td>
-                        </tr>
-                    `
-                }
-            });
 
-            document.getElementById('bodyTable').innerHTML = html;
-            document.querySelector('.table').classList.remove('hidden');
+            fetch(getUrl()+"admin/list-templates", {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                var option_html = "";
+                data.data.forEach(template => {
+                    if (template.template_name) {
+                        option_html += `<option value="${template.template_id}">${template.template_name}</option>`
+                    } else {
+                        option_html += `<option value="${template.template_id}">${template.template_id}</option>`
+                    }
+                });
+                dataMonday.data.forEach(contract => {
+                    if (contract.column_values[0].text) {
+                        html += `
+                            <tr>
+                                <td>${contract.name}</td>
+                                <td class="color_table">Template Assigned ✅</td>
+                            </tr>
+                        `
+                    } else {
+                        html += `
+                            <tr>
+                                <td>${contract.name}</td>
+                                <td>
+                                    <select id="select-${contract.id}" id-contract="${contract.id}">
+                                        <option data_placeholder="true" disabled selected style="display:none;">Select your template</option>
+                                        ${option_html}
+                                    </select>
+                                </td>
+                            </tr>
+                        `
+                    }
+                });
+                document.getElementById('bodyTable').innerHTML = html;
+                dataMonday.data.forEach(contract => {
+                    const slimSelect = new SlimSelect({
+                        select: '#select-'+contract.id,
+                        events: {
+                            afterChange: (newVal) => {
+                              slimSelect.disable();
+                              assignTemplate(slimSelect.selectEl.getAttribute("id-contract"), newVal[0].value);
+                            }
+                          }
+                    });
+                });
+                document.querySelector('.table').classList.remove('hidden');
+                loader.classList.add('hidden');
+            })
+            .catch(error => {
+                errordiv.innerHTML = error;
+                errordiv.classList.remove('hidden');
+                loader.classList.add('hidden');
+            });
 
         }else{
 
-            error.innerHTML = data.message;
-            error.classList.remove('hidden');
+            errordiv.innerHTML = data.message;
+            errordiv.classList.remove('hidden');
+            loader.classList.add('hidden');
 
         }
-        loader.classList.add('hidden');
     })
     .catch(error => {
-        error.innerHTML = error;
-        error.classList.remove('hidden');
+        errordiv.innerHTML = error;
+        errordiv.classList.remove('hidden');
         loader.classList.add('hidden');
     });
 });
+
+function assignTemplate(idContratc,idTemplate) {
+    // alert("se esta creando el contrato "+idContratc+" con la identificacion de template "+idTemplate);
+}
