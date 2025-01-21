3da528961d09aec2342b910d38ba645e93a251a0
Alejodevsoft <alejodevsoft@gmail.com>
2025-01-21 13:11:48 -0500

    Sistema de filtros

--- index.php
+++ index.php
@@ -48,52 +48,9 @@ spl_autoload_register(function ($class) {
     if (file_exists($file)) {
         require $file;
     } else {
-        echo "No se encontró el archivo: " . $file . "<br>";
+        write_warning_log("No se encontró el archivo: " . $file . "<br>");
     }
 });
-//Obtiene la ruta del navegador, limpiadno el index y la y el dominio
-function getRoute() {
-    $base_dir = rtrim(str_replace('index.php', '', $_SERVER['SCRIPT_NAME']), '/');
-    $request_uri = $_SERVER['REQUEST_URI'];
-    $relative_uri = parse_url($request_uri, PHP_URL_PATH);
-
-    if ($base_dir !== '') {
-        $relative_uri = preg_replace('#^' . preg_quote($base_dir, '#') . '/?#', '', $relative_uri);
-    }
-
-    $uri = trim($relative_uri, '/');
-
-    $return['uri']      = $uri === '' ? '/' : $uri;
-    $return['method']   = $_SERVER['REQUEST_METHOD'];
-    return $return;
-}
 
 $routesClass    = new Routes();
-
-$routes = $routesClass->getRoutes();
-// $routes = require 'routes.php';
-
-$route = getRoute();
-
-// if (array_key_exists($route, $routes)) {
-if (isset($routes[$route['method']][$route['uri']])) {
-    list($controllerClass, $method) = explode('::', $routes[$route['method']][$route['uri']]['action']);
-
-    $controller = new $controllerClass();
-    
-    if (method_exists($controller, $method)) {
-        try {
-            $controller->$method();
-        } catch (\Throwable $th) {
-            http_response_code(500);
-            write_error_log($th);
-        }
-    } else {
-        http_response_code(405);
-        echo "Ruta {$route['uri']} no encontrada.";
-        $environment!='production' ? write_error_route_log("El método $method no se encontro en el controlador $controllerClass."):'';
-    }
-} else {
-    http_response_code(404);
-    echo "404 - Página no encontrada";
-}
+$routesClass->toRoute();
